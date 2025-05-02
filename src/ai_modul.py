24b37f395cdded8c2da6aefbcaaac23e93a2a523
Tomas Svoboda <fredka171@gmail.com>
2025-05-02 16:21:43 +0200

    Update requirements and implement AI model configuration in ai_modul.py.

--- src/ai_modul.py
+++ src/ai_modul.py
@@ -1,2 +1,15 @@
 import os
+import google.generativeai as genai
 from dotenv import load_dotenv
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GEMINI_API_TOKEN"))
+
+ai_model = genai.GenerativeModel(
+    model_name = None,
+    safety_settings= None,
+    generation_config= None,
+    system_instruction= None
+)
+
+#chat_seassion = ai_model.start_chat()
